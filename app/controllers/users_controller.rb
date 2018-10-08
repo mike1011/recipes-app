3c5e99afaa8061f8f30f48c77d241fb9b62f7ae4
Gonçalo Dias da Silva <g.silva@runtime-revolution.com>
2018-10-08 18:00:16 +0100

    Adds basic controller actions for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  resources :users
   resources :recipes
 end
