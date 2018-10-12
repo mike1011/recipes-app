0bc6e71396771a3888efd56c7265b89b1c606af9
Gonçalo Dias da Silva <g.silva@runtime-revolution.com>
2018-10-12 15:04:03 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-15.times do |u|
+10.times do |u|
   user = User.create!(
-    name: "Chef #{Faker::Name.name}",
-    email: "kendrick.lamar+#{u}@example-records.com",
+    name: "Chef #{Faker::Artist.unique.name}",
+    email: "kendrick.lamar+#{u}@example-records.com"
   )
 
-  5.times do |r|
-    user.recipes.create!(
+  10.times do |r|
+    recipe = user.recipes.create!(
       title: Faker::Food.dish,
       description: Faker::Food.description,
       total_people: Faker::Number.between(1, 8),
       preparation_time: Faker::Number.between(20, 120)
     )
+
+    recipe.update_attribute(:created_at, (rand * 30).days.ago)
   end
 end
